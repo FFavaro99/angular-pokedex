be38ae2c80035dbd72a09210206d059e5c3252f1
FFavaro99 <fedefavaro99@gmail.com>
2020-08-29 14:56:34 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { PokedexComponent } from './pokedex/pokedex.component';
+import { PokemonComponent } from './pokedex/pokemon/pokemon.component';
+import { DetailsComponent } from './details/details.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PokedexComponent,
+    PokemonComponent,
+    DetailsComponent,
+    HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
